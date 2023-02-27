2dfa04e26d992df0910b86cd4d6527e886527fcb
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-02-27 19:37:45 +0900

    [FEAT] add 'POST /basic/add to save Item

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,8 +7,11 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -38,6 +41,46 @@ public class BasicItemController {
 	@GetMapping("/add")
 	public String addForm() {
 		return "basic/addForm";
+	}
+
+	// @PostMapping("/add")
+	public String addItemV1(
+		@RequestParam String itemName,
+		@RequestParam int price,
+		@RequestParam Integer quantity,
+		Model model) {
+
+		Item item = new Item();
+		item.setItemName(itemName);
+		item.setPrice(price);
+		item.setQuantity(quantity);
+
+		itemRepository.save(item);
+
+		model.addAttribute("item", item);
+
+		return "basic/item";
+	}
+
+	// @PostMapping("/add")
+	public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
+		itemRepository.save(item);
+		// model.addAttribute("item", item); 자동 추가, 생략 가능
+
+		return "basic/item";
+	}
+
+	// @PostMapping("/add")
+	public String addItemV3(@ModelAttribute Item item) {
+		itemRepository.save(item);
+		return "basic/item";
+	}
+
+	@PostMapping("/add")
+	public String addItemV4(Item item) {
+		itemRepository.save(item);
+		return "basic/item";
 	}
 
 	/**
